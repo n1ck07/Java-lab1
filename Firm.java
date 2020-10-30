5865fd67e890ea41d6f7736a13be3814f0bbdb8a
n1ck07 <48385865+n1ck07@users.noreply.github.com>
2020-10-30 10:24:18 +0200

    Create Firm.java

---
+++ Firm.java
@@ -0,0 +1,13 @@
+package Stock.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Firm {
+    public static List<String> firmlist = new ArrayList<String>();
+
+    public static void main(String[] args) {
+
+    }
+
+}
