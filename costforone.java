1681e70ef03e9441dc6474f1b05f720e6c0942ca
n1ck07 <48385865+n1ck07@users.noreply.github.com>
2020-10-30 10:25:11 +0200

    Create costforone.java

---
+++ costforone.java
@@ -0,0 +1,13 @@
+package Stock.classes;
+
+public class costforone {
+    int costforone;
+
+    public void setCostForOne(int costforone) {
+        this.costforone = costforone;
+    }
+
+    public int getCostforOne() {
+        return costforone;
+    }
+}
