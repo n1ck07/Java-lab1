218730fc558db6abfbbf183f06c3ea2f5ee82498
n1ck07 <48385865+n1ck07@users.noreply.github.com>
2020-10-30 10:26:01 +0200

    Create quantity.java

---
+++ quantity.java
@@ -0,0 +1,13 @@
+package Stock.classes;
+
+public class quantity {
+    public static int quantity;
+
+    public static int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Stock.classes.quantity.quantity = quantity;
+    }
+}
